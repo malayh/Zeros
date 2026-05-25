ea54d0cfb5130039cc62e0a2ae1135516ab02e42
malayh <malay.hazarika@gmail.com>
2026-05-25 09:46:02 +0530

    audio config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,6 +59,14 @@ in
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+    "context.properties" = {
+      "default.clock.rate" = 48000;
+      "default.clock.quantum" = 64;
+      "default.clock.min-quantum" = 32;
+      "default.clock.max-quantum" = 256;
+    };
+  };
   };
 
   services.xserver.xkb = {
